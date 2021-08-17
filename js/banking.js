29bbb30f972ed2d8c59088fd7313491cda6b92a1
nabila13 <43751134+nabila13@users.noreply.github.com>
2021-08-17 15:24:00 +0600

    functional update

--- js/banking.js
+++ js/banking.js
@@ -1,29 +1,71 @@
-const submitDeposit = document.getElementById("submit-deposit");
-const submitWithdraw = document.getElementById("submit-withdraw");
-const deposit = document.getElementById("deposit");
-const withdraw = document.getElementById("withdraw");
-const totalAmount = document.getElementById("amount3");
-submitDeposit.addEventListener('click', function() {
+function getInputValue(inputID) {
+    const inputs = document.getElementById(inputID);
+    const inputsText = inputs.value;
+    const inputVal = parseFloat(inputsText);
+    inputs.value = "";
+    return inputVal;
+}
 
-    const previousDeposit = document.getElementById("amount").innerText;
-    const newDeposit = deposit.value;
-    document.getElementById("amount").innerText = parseFloat(newDeposit) + parseFloat(previousDeposit);
-    totalAmount.innerText = parseFloat(totalAmount.innerText) + parseFloat(newDeposit);
+function addValue(updateID, value) {
+    const update = document.getElementById(updateID);
+    const updateText = update.innerText;
+    const updateValue = parseFloat(updateText);
+    const totalAmountVal = getBalanceValue();
+    if (value <= totalAmountVal) {
+        const newUpdate = updateValue + value;
+        update.innerText = newUpdate;
+    }
+}
 
+function getBalanceValue() {
+    const totalAmount = document.getElementById("amount3");
+    const totalAmountText = totalAmount.innerText;
+    const totalAmountVal = parseFloat(totalAmountText);
+    return totalAmountVal;
+}
+
+function updateBalance(updateAmount, isAdd) {
+
+    let totalAmountv = getBalanceValue();
+    if (isAdd === true) {
+        totalAmountv = totalAmountv + updateAmount;
+    } else {
+        if (updateAmount <= totalAmountv) {
+            totalAmountv = totalAmountv - updateAmount;
+        }
+    }
+    document.getElementById("amount3").innerText = totalAmountv;
+}
+document.getElementById("submit-deposit").addEventListener('click', function() {
+    const previousDeposit = "amount";
+    const deposit = "deposit";
+    const newDeposit = getInputValue(deposit);
+
+    if (newDeposit > 0) {
+        addValue(previousDeposit, newDeposit);
+        updateBalance(newDeposit, true);
+    }
+
+    // totalAmount.innerText = parseFloat(totalAmount.innerText) + newDeposit;
 });
 
-submitWithdraw.addEventListener("click", function() {
-
-    const previousWithdraw = document.getElementById("amount2").innerText;
-    const newWithdraw = withdraw.value;
-    document.getElementById("amount2").innerText = parseFloat(newWithdraw) + parseFloat(previousWithdraw);
-    const total = parseFloat(totalAmount.innerText) - parseFloat(newWithdraw);
-    if (total <= 0) {
-        submitWithdraw.disabled = true;
-        submitWithdraw.style.backgroundColor = "bg-red-200";
-        totalAmount.innerText = 0;
-    } else {
-        totalAmount.innerText = total;
-        submitWithdraw.disabled = false;
+document.getElementById("submit-withdraw").addEventListener("click", function() {
+    const withdraw = "withdraw";
+    const previousWithdraw = "amount2";
+    const newWithdraw = getInputValue(withdraw);
+    if (newWithdraw > 0) {
+        addValue(previousWithdraw, newWithdraw);
+        updateBalance(newWithdraw, false);
     }
+
+    // totalAmount.innerText = parseFloat(totalAmount.innerText) - newWithdraw;
+    // if (total <= 0) {
+    //     submitWithdraw.disabled = true;
+    //     submitWithdraw.classList.add("bg-red-100");
+    //     totalAmount.innerText = 0;
+    // } else {
+    //     totalAmount.innerText = total;
+    //     submitWithdraw.disabled = false;
+    //     submitWithdraw.classList.remove("bg-red-100");
+    // }
 });
